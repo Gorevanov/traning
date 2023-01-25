b00af50b44516519151e38f9a951a5e667589c8d
Gorevanov Pavel <petyagorevanov@gmail.com>
2023-01-25 11:59:41 +0500

    class2

--- traning_class.py
+++ traning_class.py
@@ -16,3 +16,26 @@ car3 = car("toyota", "land cruser", 2001)
 car1.description_car()
 
 print(car3.market)
+print()
+
+
+class Human:
+    age = None
+
+    def __init__(self, age=4):
+        self.age = age
+
+    # добавляем геттер - специальный метод для получения поля
+    def get_age(self):
+        return self.age
+
+    # добавляем сеттер - специальный метод для установки нового значения
+    def set_age(self, age):
+        if age > 0 and isinstance(age,
+                                  int):  # проверяем условия, что человеку должно быть больше 0 лет и его возраст - целое число
+            self.age = age
+
+
+h = Human()
+h.set_age(0)
+print(h.get_age())
